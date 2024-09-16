05786b96a3c0e85cb2bcd25fb00ad5645020bee4
Lauren Perry <lgpperry@gmail.com>
2024-09-16 11:41:13 -0700

    Update data_s24.R

--- teaching/stat1/data/data_s24.R
+++ teaching/stat1/data/data_s24.R
@@ -1,2 +1,2 @@
-survey <- read.delim("https://lgpperry.github.io/teaching/stat1/data/Stat1_SurveyData_S24.csv")
+survey <- read.csv("https://lgpperry.github.io/teaching/stat1/data/Stat1_SurveyData_S24.csv")
 survey[sapply(survey, is.character)] <- lapply(survey[sapply(survey, is.character)], as.factor)
